e4486ac59922a8e5ab8e28c3229a0e452bfa352b
kwang-hoon <43735944+kwang-hoon@users.noreply.github.com>
2019-02-08 21:20:09 +0900

    Add files via upload

--- dormitory_web/src/Components/Bottom.vue
+++ dormitory_web/src/Components/Bottom.vue
@@ -3,10 +3,10 @@
         <h3 class="menutitle">식단 입력</h3>
         <calender></calender>
         <h3 class="memotitle">메모 작성</h3>
-        <textarea v-model="addinfo" class="addinfo"></textarea>
+        <textarea class="addinfo"></textarea>
         <div class="save">
-            <button v-on:click="imgsave" class="imgsave">이미지로 저장하기</button>
-            <button v-on:click="svsave" class="svsave">서버에 저장하기</button>
+            <button @click="imgsave()" class="imgsave">이미지로 저장하기</button>
+            <button @click="svsave()" class="svsave">서버에 저장하기</button>
         </div>
     </div>
 </template>
@@ -17,8 +17,31 @@ import calender from './calender'
 export default {
     name: 'Bottom',
 
+    data: function() {
+        return{
+            how: 'logout'
+        }
+    },
+
     components: {
         calender
+    },
+    methods: {
+        imgsave() {
+            if(this.token == this.how){
+                alert("로그인 해주세요")
+            }
+        },
+        svsave() {
+            if(this.token == this.how){
+                alert("로그인 해주세요")
+            }
+        }     
+    },
+    computed: {
+        token: function() {
+            return this.$store.state.token
+        }
     }
 }
 </script>
--- dormitory_web/src/main.js
+++ dormitory_web/src/main.js
@@ -8,11 +8,32 @@ import axios from 'axios'
 import App from '@/App.vue'
 import router from '@/routes'
 
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    token: 'logout'
+  },
+  mutations: {
+    setToken: function(state, data){
+      state.token = data
+    }
+  },
+  actions: {
+
+  },
+})
+
+
+
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
 
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
