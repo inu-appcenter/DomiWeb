d17c28191f6c01b5347357fb91ad185af99ebc73
kwang-hoon <43735944+kwang-hoon@users.noreply.github.com>
2019-02-09 18:28:01 +0900

    지난주/다음주 버튼 오류 해결

--- dormitory_web/src/Components/calender.vue
+++ dormitory_web/src/Components/calender.vue
@@ -1,18 +1,21 @@
 <template>
     <div id="calender">
-        <button @click="lastweek()" class="btn-last">{{btn_lastweek}}</button>
+        <button @click="lastweek()" class="btn-last" data-html2canvas-ignore="true">{{btn_lastweek}}</button>
         <span class="week">{{week1}}_{{week2}}</span>
-        <button @click="nextweek()" class="btn-next">{{btn_nextweek}}</button><br>
+        <button @click="nextweek()" class="btn-next" data-html2canvas-ignore="true">{{btn_nextweek}}</button><br>
     </div>
 </template>
 
 <script>
+
+var today = new Date() // 0000.00.00 ~ 0000.00.00 앞부분
+var after = new Date() // 0000.00.00 ~ 0000.00.00 뒷부분
+
 export default {
     name: "calender",
 
     created(){
-        var today = new Date() // 0000.00.00 ~ 0000.00.00 앞부분
-        var after = new Date() // 0000.00.00 ~ 0000.00.00 뒷부분
+        
         var dateOffset = (24*60*60*1000) * 6
 
         //현재주의 월요일 날짜
@@ -68,6 +71,46 @@ export default {
             var dateOffset2 = (24*60*60*1000) * 7
 
             today.setTime(today.getTime() - dateOffset2)
+                       
+            var mon = today.getDay() || 7;
+            if( mon !== 1){
+                today.setHours(-24 * (mon-1));
+            }
+
+            var dd1 = today.getDate()
+            var mm1 = today.getMonth()+1
+            var yyyy1 = today.getFullYear()
+
+            //yyyy.mm.dd
+            if(dd1<10) {
+                dd1='0'+dd1
+            }
+            if(mm1<10) {
+                mm1='0'+mm1
+            }
+
+            this.week1 = yyyy1+'.'+mm1+'.'+dd1
+            
+
+            after.setTime(today.getTime() + dateOffset1)
+            var dd2 = after.getDate();
+            var mm2 = after.getMonth() +1
+            var yyyy2 = after.getFullYear()
+
+            if(dd2<10) {
+                dd2='0'+dd2
+            }
+            if(mm2<10) {
+                mm2='0'+mm2
+            }
+
+            this.week2 = yyyy2+'.'+mm2+'.'+dd2
+        },
+        nextweek(){
+            var dateOffset1 = (24*60*60*1000) * 6
+            var dateOffset2 = (24*60*60*1000) * 7
+
+            today.setTime(today.getTime() + dateOffset2)
                        
             var mon = today.getDay() || 7;
             if( mon !== 1){
