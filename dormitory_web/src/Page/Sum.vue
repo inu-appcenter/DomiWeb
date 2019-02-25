29963ce2eb5a3fe264ca58083b91a1708d269808
SungMinKim <>
2019-02-25 19:32:42 +0900

    axios

--- dormitory_web/src/Page/Sum.vue
+++ dormitory_web/src/Page/Sum.vue
@@ -15,6 +15,8 @@ export default {
 
     created: function(){
         this.getOneDayMenu()
+        this.getTodayMenu()
+        this.getThisWeekendMenu()
     },
 
     components: {
@@ -26,7 +28,35 @@ export default {
         getOneDayMenu(){
             var self = this
 
-            axios.get('http://117.16.191.242:5630/read/date')
+            axios.get('http://117.16.191.242:5630/read/date',
+            {date: 20190207})
+            .then(function(response) {
+
+                console.log(response)
+                })
+                .catch(function(error) {
+                    console.log(error)
+                    })
+        },
+
+        getTodayMenu(){
+            var self = this
+
+            axios.get('http://117.16.191.242:5630/read',
+            {date: 20190207})
+            .then(function(response) {
+
+                console.log(response)
+                })
+                .catch(function(error) {
+                    console.log(error)
+                    })
+        },
+
+        getThisWeekendMenu(){
+            var self = this
+            axios.post('http://117.16.191.242:5630/read/all',
+            {date: [20190204, 20190205, 20190206, 20190207, 20190208, 20190209]})
             .then(function(response) {
                 console.log(response)
                 })
