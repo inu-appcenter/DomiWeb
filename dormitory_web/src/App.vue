91f43cabc124a522e626786d538c254123fff26e
SungMinKim <>
2019-02-25 16:58:47 +0900

    modify code

--- dormitory_web/src/App.vue
+++ dormitory_web/src/App.vue
@@ -12,9 +12,6 @@ import axios from 'axios'
 export default {
   name: 'app',
 
-  created: function(){
-  },
-
   components: {
     Sum,
   }
--- dormitory_web/src/main.js
+++ dormitory_web/src/main.js
@@ -7,28 +7,6 @@ import Vue from 'vue'
 import axios from 'axios'
 import App from '@/App.vue'
 import router from '@/routes'
-import Vuex from 'vuex'
-
-Vue.prototype.$http = axios
-
-
-Vue.use(Vuex)
-
-const store = new Vuex.Store({
-  state: {
-    token: 'logout'
-  },
-  mutations: {
-    setToken: function(state, data){
-      state.token = data
-    }
-  },
-  actions: {
-
-  },
-})
-
-
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
@@ -36,12 +14,6 @@ Vue.prototype.$http = axios
 new Vue({
   el: '#app',
   router,
-<<<<<<< HEAD
   components: {App},
   template: '<App/>',
 })
-=======
-  store,
-  render: h => h(App)
-})
->>>>>>> 5628dd75ab8a8a36c95d9fee7224d72ea271c307
